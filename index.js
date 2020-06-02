7cb607e8e19caab07c9a8b66ba2a84e4ddd3d36c
p.sagitza <p.sagitza@loom.de>
2020-06-02 12:11:06 +0200

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+const Path = require('path');
+const FS = require('fs');
+
+module.exports = class GloomPlugin {
+
+  constructor(path, configs = {}) {
+    this._path = path;
+    this._configs = configs;
+    this._plugins = null;
+
+    this.load();
+  }
+
+  getPath() {
+    if (Path.isAbsolute(this._path)) {
+      return this._path;
+    }
+    return Path.join(process.cwd(), this._path);
+  }
+
+  getPlugin(name) {
+    return this._plugins[name] || null;
+  }
+
+  load() {
+    if (this._plugins !== null) return;
+    this._plugins = {};
+    const base = this.getPath();
+    const list = FS.readdirSync(base);
+
+    for (const file of list) {
+      this._plugins[Path.basename(file)] = require(Path.join(base, file));
+    }
+    return this;
+  }
+
+  init() {
+    this.load();
+    for (const plugin in this._plugins) {
+      this._plugins[plugin](this._configs, this);
+    }
+  }
+
+}
